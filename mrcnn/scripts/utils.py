bb09bb9dc6e4dbd3915f453c875a215768bc1364
pshivraj <shivrajpurshottam@gmail.com>
2019-03-11 18:46:56 +0000

    Added weighted Iou upto 0.75

--- mrcnn/scripts/utils.py
+++ mrcnn/scripts/utils.py
@@ -733,8 +733,9 @@ def compute_ap_range(gt_box, gt_class_id, gt_mask,
                      iou_thresholds=None, verbose=1):
     """Compute AP over a range or IoU thresholds. Default range is 0.5-0.95."""
     # Default is 0.5 to 0.95 with increments of 0.05
-    iou_thresholds = iou_thresholds or np.arange(0.5, 1.0, 0.05)
-    
+    iou_thresholds = iou_thresholds or np.arange(0.5, 0.80, 0.05)
+    weighted_threshold = np.arange(len(iou_thresholds), 0, -1)
+    weighted_threshold = weighted_threshold/sum(weighted_threshold)
     # Compute AP over range of IoU thresholds
     AP = []
     for iou_threshold in iou_thresholds:
@@ -745,7 +746,7 @@ def compute_ap_range(gt_box, gt_class_id, gt_mask,
         if verbose:
             print("AP @{:.2f}:\t {:.3f}".format(iou_threshold, ap))
         AP.append(ap)
-    AP = np.array(AP).mean()
+    AP = np.dot(np.array(AP), weighted_threshold)
     if verbose:
         print("AP @{:.2f}-{:.2f}:\t {:.3f}".format(
             iou_thresholds[0], iou_thresholds[-1], AP))
